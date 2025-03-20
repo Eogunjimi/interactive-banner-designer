348d07e6e0c37d0ce98c0c9a2a43ef536525bb5c
Emmanuel <eogunjimi82@gmail.com>
2025-03-20 19:39:39 +0100

    update content in the context

--- src/Context/EditorContext.jsx
+++ src/Context/EditorContext.jsx
@@ -4,8 +4,8 @@ export const EditorContext = createContext();
 
 const EditorContextProvider = (props) => {
 
-    const [title, setTitle] = useState('Emmanuel')
-    const [body, setBody] = useState("Coding has been an exhilarating adventure! Every bug fixed, every challenge solved—it’s like unlocking new levels of creativity.")
+    const [title, setTitle] = useState(' The Magic of Simplicity')
+    const [body, setBody] = useState("Sometimes, it's the little things that bring the most joy—a quiet morning, a good book, or a moment of stillness. Life doesn’t need to be complicated to be beautiful. Take a step back, enjoy the simple moments, and watch them light up your world.")
     const [footer, setFooter] =useState("Emmanuel Ogunjimi")
     const [color, setColor] = useState('#BB3ECC');
 
