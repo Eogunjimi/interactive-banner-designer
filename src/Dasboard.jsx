426165b348e8dc5eb0a0e85e39099a6dc23cc023
Emmanuel <eogunjimi82@gmail.com>
2025-04-17 18:51:04 +0100

    Code clean up, fixed the bugs that stopping the page from displaying

--- src/Dasboard.jsx
+++ src/Dasboard.jsx
@@ -1,34 +1,21 @@
 import Navbar from "./Components/Navbar";
 import Pagetitle from "./Components/Pagetitle";
-import NewAds from "./NewAds";
 import Banner from "./Pages/Banner";
 import Editor from "./Pages/Editor";
-// import TestBanner from "./Pages/TestBanner";
- 
-// import BannerPro from "./Pages/BannerPro";
 
-// bg-[#f4f4f4]
 
 const Dashboard = () => {
   return (
     <div className="h-screen pb-10 overflow-hidden">
-      <div className="h-full"> 
-      <Navbar />
-
-       <div className="flex justify-between mx- h-full">
-         <Editor/> 
-         
-
-         <div className="w-full bg-[#f4f4f4]" >
-          <Pagetitle/>
-           
-           <Banner />
-           {/* <NewAds/> */}
-           {/* <TestBanner/> */}
-            
-         </div>
-        
-      </div>
+      <div className="h-full">
+        <Navbar />
+        <div className="  flex justify-between mx- h-full ">
+          <Editor />
+          <div className="w-full bg-[#f4f4f4]" > 
+            <Pagetitle />
+            <Banner />
+          </div>
+        </div>
       </div>
     </div>
   );
