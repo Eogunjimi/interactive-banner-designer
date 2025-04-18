f9a21ebec2fd7a46dd45c1f545bbab02aad09684
Emmanuel <eogunjimi82@gmail.com>
2025-04-18 10:16:36 +0100

    Implemented clean code concept for easy to read code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,11 +4,13 @@ import { SiAnycubic } from "react-icons/si";
 const Navbar = () => {
   return (
     <>
-      <div className="flex justify-between items-center px-10 bg-white h-10 my- mx- rounded-sm">
+      <div className="flex justify-between items-center px-10
+       bg-white h-10 my- mx- rounded-sm">
         <div>
           <SiAnycubic />
         </div>
-        <div className="border-1 border-gray-300 py-1 px-2 flex justify-center items-center rounded-full gap-1">
+        <div className="border-1 border-gray-300 py-1 px-2 
+        flex justify-center items-center rounded-full gap-1">
           <FaUser />
           <p className="text-sm">folusho jimi</p>
         </div>
--- src/tests/Banner.test.jsx
+++ src/tests/Banner.test.jsx
@@ -8,39 +8,3 @@ describe('campaign Banner renders', () =>{
       expect(screen.getByTestId("campaign-banner")).toBeInTheDocument();
     });
 });
-
-
-
-
-// describe("Banner container renders", () => {
-//     it("Ensure the banner container renders on he screen", () => {
-//       render(<Banner />);
-//       expect(screen.getByTestId("banner-container")).toBeInTheDocument();
-//     });
-//   });
-  
-
-// import {describe, it, expect} from 'vitest';
-// describe('something truthy and falsy', () => {
-//     it('true to be true', ()=>{
-//         expect(true).toBe(true);
-//     });
-    
-//     it('false to be false', ()=>{
-//         expect(false).toBe(false);
-//     });
-// });
-
-//My Test Practice
-// describe('checking an array for certain item', ()=>{
-//     it('check if banana is there', ()=>{
-      
-//         const fruite = ['mango', 'banana']
-
-//         expect(fruite).toContain('mango')
-//     });
-// });
-
-
-
- 
